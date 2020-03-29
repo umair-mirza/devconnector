9a05958076160da7319ec5fd3d79af3087dbecdb
Umair Mirza <umair_hp@hotmail.com>
2020-03-29 17:24:56 +0500

    Prepared for Deployment

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-const db = config.get('mongoURI');
+const db = config.get('mongoURI').toString();
 
 //we will use async awat instead of promises because its new and more advanced
 
 const connectDB = async () => {
     try {
         mongoose.Promise = global.Promise;
-        await mongoose.connect(db, {useMongoClient: true});
+        await mongoose.connect(db, {useNewUrlParser: true});
 
         console.log("MongoDB connected");
     }
